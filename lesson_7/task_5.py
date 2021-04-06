ab3eab176e0fddbe33999950fc3987a9dd2f0ea8
Andrey <mybelovedsolidsun@gmail.com>
2021-04-06 23:19:23 +0300

    solutions for 7th lesson

---
+++ lesson_7/task_5.py
@@ -0,0 +1,20 @@
+import os
+stat_dict: dict = {10 ** i: [0, []] for i in range(1, 6)}
+
+
+if __name__ == '__main__':
+    os.chdir('../')
+
+    for root, folder, files in os.walk('.'):
+        for file in files:
+            size = os.stat(os.path.join(root, file)).st_size
+            counter = 1
+            while counter < 6:
+                if size % (10 ** counter) == 0:
+                    stat_dict[(10 ** counter)][0] += 1
+                    if len(file.split('.')) > 1 and file.split('.')[1] not in stat_dict[(10 ** counter)][1]:
+                        stat_dict[(10 ** counter)][1].append(file.split('.')[1])
+                counter += 1
+
+    for (index, item) in stat_dict.items():
+        print(f'{index} : ({item[0]}, {item[1]})')
---
+++ main.py
@@ -0,0 +1,4 @@
+import os
+
+for i in os.walk(os.path.curdir + r'\lesson_6'):
+    print(i)
